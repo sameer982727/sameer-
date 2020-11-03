68b0004c9a478547002a265f06fd04222b553e20
sameer982727 <69586075+sameer982727@users.noreply.github.com>
2020-11-03 18:36:00 +0530

    Add files via upload

---
+++ matrices/symmetric matrix.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+class symatrix
+{
+    public:
+          int ma[10];
+          void insert(int e,int a,int b)
+          { 
+             if(a<1||b<1||a>10||b>10)
+             {
+                 cout<<"Array out of index"<<endl;
+             }
+            else
+			{
+			  
+             ma[(b*(b-1))/2+a]=e;   
+          }
+     }
+		  void display(int size)
+          {
+              for(int i=1;i<=size;i++)
+              {
+                  for(int j=1;j<=size;j++)
+                  {
+                      if(i<=j)
+                      {
+                          cout<<ma[(j*(j-1))/2+i]<<"  ";
+                      }
+                      else
+                      {
+                          cout<<ma[(i*(i-1))/2+j]<<"  ";
+                      }
+                  }
+                  cout<<endl<<endl;
+              }
+          }
+           int retrieve(int i,int j,int size)
+          {
+              if(i<=j&&i<=size&&j<=size)
+              {
+                  return ma[(j*(j-1))/2+i];
+              }
+              else 
+              if(i<1||j<1||i>size||j>size)
+              {
+                cout<<"Invalid row and column input"<<endl;
+                exit(0);
+              }
+              else 
+              {
+                  return ma[(i*(i-1))/2+j];
+              }
+          }  
+};
+int main()
+{   
+    int size,e,i,j,a,b;
+    symatrix s;
+    cout<<"Enter the no of rows and columns in  matrx "<<endl;
+    cin>>size;
+    for(i=1,j=1;i<=size,j<=size;i++,j++)
+    {
+        cout<<"Enter diagonal element of index ("<<i<<","<<j<<")"<<endl;
+        cin>>e;
+        s.insert(e,i,j);
+    }
+    cout<<"Your entered diagonal matrix is: "<<endl;
+    s.display(size);
+    cout<<"Enter row from which u want to retrieve the element: "<<endl;
+    cin>>a;
+    cout<<"Enter column from which u want to retrieve the element: "<<endl;
+    cin>>b;
+    cout<<"Retrieved element is: "<<s.retrieve(a,b,size)<<endl;
+    return 0;
+}
